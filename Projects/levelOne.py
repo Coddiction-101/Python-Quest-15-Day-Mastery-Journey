3746be83eefc3ce51491fa0ad46f91244b2851b7
Gulshan Kushwaha <Gulshansays333@gmail.com>
2026-01-12 20:16:33 +0530

    Update and rename fProject.py to levelOne.py

---
+++ Projects/levelOne.py
@@ -0,0 +1,17 @@
+num1 = input("Enter first number:")
+num2 = input("Enter second number:")
+
+
+n1 = float(num1)
+n2 = float(num2)
+
+sum = n1 + n2
+diff = n1 - n2
+prod = n1 * n2
+div = n1 / n2
+
+
+print("sum:", sum)
+print("diff:", diff)
+print("prod:", prod)
+print("div:", div)
